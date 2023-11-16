9a7ead8b57b6da31d701f987324d5ddbf3b5b3fa
unknown <pipicoker@gmail.com>
2023-11-16 11:17:45 +0100

    addede link to the logout button

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -37,16 +37,20 @@ const Sidebar = () => {
     </div>
 
         <div className='mt-[200px] divide-y '>
+          <Link to="/"
+
+          >
+
             <button className='flex items-center  gap-3 font-semibold text-base bg-greyish w-full  rounded-[4px]  py-3 px-4 mb-11 text-greyish2'>
                 <FiLogOut className=''/>
                 Logout
             </button>
-        
+          </Link>
             <div className='pt-8 flex items-center space-x-3'>
                 <img src="https://media.istockphoto.com/id/1406308214/photo/portrait-of-mature-multiracial-asian-businessman-in-office-hallway-smiling.jpg?s=612x612&w=0&k=20&c=uIhJqBL2lyg4zZN0Mdx7jHARJiA3NJCE66IPYu6l93M=" alt="" className='w-8 h-8 rounded-[32px]'/>
 
                 <div className=''>
-                    <p className='font-semibold text-[#FFF]'>Tanzir Rahman</p>
+                    <p className='font-semibold text-[#FFF]'>Princess Ekere</p>
                     <p className='text-xs text-greyish2 flex justify-start'>View Profile</p>
                 </div>
             </div>
